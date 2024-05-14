5f44480dc76b7503223a11409dde9a7725ff15d3
8rs1 <arssani.info@gmail.com>
2024-05-14 19:11:53 +0330

    refactor: use functional programming

--- index.js
+++ index.js
@@ -1,141 +1,97 @@
-let outname = document.querySelector(".out-name")
-let inpname = document.querySelector(".inp-name")
-function updateValuename(){
-    let dd = inpname.value;
-    outname.innerText = dd
-}
-let outnumber = document.querySelector(".out-number")
-let inpnumber = document.querySelector(".inp-number")
-function updateValuenumber(){
-    let aa = inpnumber.value;
-    outnumber.innerText = aa
-}
-let outdate1 = document.querySelector(".out-date1")
-let inpdate1 = document.querySelector(".inp-date1")
-function updateValuedate1(){
-    let cc1 = inpdate1.value;
-    outdate1.innerText = cc1
-}
-let outdate2 = document.querySelector(".out-date2")
-let inpdate2 = document.querySelector(".inp-date2")
-function updateValuedate2(){
-    let cc2 = inpdate2.value;
-    outdate2.innerText = cc2
-}
-let outcv2 = document.querySelector(".out-cv2")
-let inpcv2 = document.querySelector(".inp-cv2")
-function updateValuecv22(){
-    let gg = inpcv2.value;
-    outcv2.innerText = gg
-}
-const separateByFour = (str) => {
-    return str ? str.match(/.{1,4}/g).join(" "):''
-}
-inpnumber.addEventListener("input",function(e){
-    let result = separateByFour(e.target.value)
-    outnumber.textContent = result
-})
+const $_ = className => document.querySelector(`.${className}`);
 
+const updateValue = (inp, out) => {
+  const { value } = inp;
+  out.innerText = value;
+};
 
+const setValueToEmpty = elem => (elem.textContent = "");
 
-let inputname = document.querySelector(".inp-name")
-let pErrorname = document.querySelector(".red-error__name")
-// 
-let inputnumber = document.querySelector(".inp-number")
-let pErrornumber = document.querySelector(".red-error__number")
-// 
-let inputdate1 = document.querySelector(".inp-date1")
-let inputdate2 = document.querySelector(".inp-date2")
-let pErrordate = document.querySelector(".red-error__date")
-// 
-let inputcv2 = document.querySelector(".inp-cv2")
-let pErrorcv2 = document.querySelector(".red-error__cv2")
-let button = document.querySelector(".click-b")
-inputname.addEventListener('input', function () {
-    pErrorname.textContent = "";
-})
-inputnumber.addEventListener('input', function(){
-    pErrornumber.textContent = '';
-})
-inputdate1.addEventListener('input', function(){
-    pErrordate.textContent = '';
-})
-inputcv2.addEventListener('input', function(){
-    pErrorcv2.textContent = '';
-})
-button.addEventListener("click", function(event) {
-    event.preventDefault();
-    let username = inputname.value.trim();
-    let usernumber = inputnumber.value.trim();
-    let userdate1 = inputdate1.value.trim();
-    let userdate2 = inputdate2.value.trim();
-    let usercv2 = inputcv2.value.trim();
-    let isvalid = true
-    if (username === '') {
-        pErrorname.textContent = 'Writing the name and surname is mandatory!';
-        isvalid = false;
-    }else if (!/^[A-Za-z\s]+$/.test(username)) {
-        pErrorname.textContent = 'Use only letters!'
-        isvalid = false;
-    }else if (username.length < 5) {
-        pErrorname.textContent = 'Write the full name and surname!'
-        isvalid = false;
-    } else {
-        pErrorname.textContent = "";
-    }
-    if (usernumber === '') {
-        pErrornumber.textContent = 'It is mandatory to write the card number!';
-        isvalid = false;
-    }else if (!/^[0-9\s]+$/.test(usernumber)) {
-        pErrornumber.textContent = 'Use only numbers!'
-        isvalid = false;
-    }else if (usernumber.length < 16) {
-        pErrornumber.textContent = 'Enter the card number in full!'
-        isvalid = false;
-    }else {
-        pErrornumber.textContent = "";
-    }
-    if (userdate1 === '') {
-        pErrordate.textContent = 'is nessessary!';
-        isvalid = false;
-    }else if (!/^[0-9]+$/.test(userdate1)) {
-        pErrordate.textContent = 'Use only numbers!'
-        isvalid = false;
-    }else if (userdate1.length < 2) {
-        pErrordate.textContent = 'Please write the date in full!'
-        isvalid = false;
-    }else {
-        pErrordate.textContent = "";
-    }
-    if (userdate2 === '') {
-        pErrordate.textContent = 'is nessessary!';
-        isvalid = false;
-    }else if (!/^[0-9]+$/.test(userdate2)) {
-        pErrordate.textContent = 'Use only numbers!'
-        isvalid = false;
-    }else if (userdate2.length < 2) {
-        pErrordate.textContent = 'Please write the date in full!'
-        isvalid = false;
-    }else {
-        pErrordate.textContent = "";
-    }
-    if (usercv2 === '') {
-        pErrorcv2.textContent = 'is nessessary!';
-        isvalid = false;
-    }else if (!/^[0-9]+$/.test(usercv2)) {
-        pErrorcv2.textContent = 'Use only numbers!'
-        isvalid = false;
-    }else if (usercv2.length < 3) {
-        pErrorcv2.textContent = 'Please write the date in full!'
-        isvalid = false;
-    }else {
-        pErrorcv2.textContent = "";
-    }
-    if (isvalid) {
-        form.style.display = "none"
-        formcomplet.style.display = "block"
-        formcomplet.style.display = "flex"
-    }
-})
-let form = document.querySelector(".form")
-let formcomplet = document.querySelector(".form-complet")
+const setValue = (elem, value) => (elem.textContent = value);
+
+const isNumber = str => {
+  const numberRegEx = /^[0-9\s]+$/;
+  return numberRegEx.test(str);
+};
+
+const elemValue = elem => elem.value.trim()
+
+const dateErrorElem = $_("red-error__date");
+
+const nameElem = { input: $_("inp-name"), output: $_("out-name"), error: $_("red-error__name") };
+const numberElem = { input: $_("inp-number"), output: $_("out-number"), error: $_("red-error__number") };
+const date1Elem = { input: $_("inp-date1"), output: $_("out-date1"), error: dateErrorElem };
+const date2Elem = { input: $_("inp_date2"), output: $_("out-date2"), error: dateErrorElem };
+const cvElem = { input: $_("inp-cv2"), output: $_("out-cv2"), error: $_("red-error__cv2") };
+
+const separateByFour = str => {
+  return str ? str.match(/.{1,4}/g).join(" ") : "";
+};
+nameElem.input.addEventListener("input", e => {
+  let result = separateByFour(e.target.value);
+  outnumber.textContent = result;
+});
+
+let button = $_("click-b");
+
+nameElem.input.addEventListener("input", () => setValueToEmpty(nameElem.error));
+numberElem.input.addEventListener("input", () => setValueToEmpty(numberElem.error));
+date1Elem.input.addEventListener("input", () => setValueToEmpty(date1Elem.error));
+cvElem.input.addEventListener("input", () => setValueToEmpty(cvElem.error));
+
+button.addEventListener("click", event => {
+  event.preventDefault();
+  let username = elemValue(nameElem.input);
+  let usernumber = elemValue(numberElem.input);
+  let userdate1 = elemValue(date1Elem.input);
+  let userdate2 = elemValue(date2Elem.input);
+  let usercv2 = elemValue(cvElem.input);
+  let isvalid = false;
+  if (!username) {
+    setValue(nameElem.error, "Writing the name and surname is mandatory!");
+  } else if (!/^[\w\s]+$/.test(username)) {
+    setValue(nameElem.error, "Use only letters!");
+  } else if (username.length < 5) {
+    setValue(nameElem.error, "Write the full name and surname!");
+  } else {
+    setValueToEmpty(nameElem.error);
+    isvalid = true;
+  }
+  if (!usernumber) {
+    setValue(numberElem.error, "It is mandatory to write the card number!");
+  } else if (!isNumber(usernumber)) {
+    setValue(numberElem.error, "Use only numbers!");
+  } else if (usernumber.length < 16) {
+    setValue(numberElem.error, "Enter the card number in full!");
+  } else {
+    setValueToEmpty(numberElem.error);
+    isvalid = true;
+  }
+  if (!userdate1 || !userdate2) {
+    setValue(dateErrorElem, "is nessessary!");
+  } else if (!isNumber(userdate1) || !isNumber(userdate2)) {
+    setValue(dateErrorElem, "Use only numbers!");
+  } else if (userdate1.length < 2 || userdate2.length < 2) {
+    setValue(dateErrorElem, "Please write the date in full!");
+  } else {
+    setValueToEmpty(dateErrorElem);
+    isvalid = true;
+  }
+  if (!usercv2) {
+    setValue(cvElem.error, "is nessessary!");
+  } else if (!isNumber(usercv2)) {
+    setValue(cvElem.error, "Use only numbers!");
+  } else if (usercv2.length < 3) {
+    setValue(cvElem.error, "Please write the date in full!");
+  } else {
+    setValueToEmpty(cvElem.error);
+    isvalid = true;
+  }
+  if (isvalid) {
+    form.classList.add("hidden");
+    formcomplet.classList.add("block");
+    formcomplet.classList.add("flex");
+  }
+});
+let form = $_("form");
+let formcomplet = $_("form-complet");
